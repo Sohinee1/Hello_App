d35e5ae8997b12426543251232c042b9d1ed146c
Sohinee1 <sc5248@srmist.edu.in>
2026-04-06 08:30:53 +0530

    Added UC3

--- Hello_App/src/main/java/org/example/uc2_HelloApp.java
+++ Hello_App/src/main/java/org/example/uc2_HelloApp.java
@@ -1,12 +1,14 @@
 package org.example;
 
-    public class UC2 {
-        public static void main(String[] args){
 
-            // Read name from command line argument
-            String name = args[0];
-
-            // Display greeting
-            System.out.println("Hello, " + name + "!");
+public class UC3 {
+    public static void main(String[] args){
+        String name="World";
+        if(args.length > 0){
+            name=args[0];
         }
+        System.out.println("Hello,"+name+"!");
     }
+
+
+}
